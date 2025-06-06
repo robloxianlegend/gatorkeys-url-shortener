3b04ee6d7220b4878497654808c81c9f43cfd665
unstable <dthebestusernamelol+WHATTHESIGMA@gmail.com>
2025-06-06 19:04:47 +0800

    #

--- [code].js
+++ [code].js
@@ -1,8 +1,8 @@
 let links = {}
 
 export default async function handler(req, res) {
-  const code = req.query.code
-  if (!links[code]) return res.status(404).send('Not found')
+  const code = req.query.code || req.url.split('/').pop()
+  if (!code || !links[code]) return res.status(404).send('Not found')
 
   res.writeHead(302, { Location: links[code] })
   res.end()
