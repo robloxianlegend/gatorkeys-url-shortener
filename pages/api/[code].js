36ca46443ab76223af3bc2a1284a3089e461d051
unstable <dthebestusernamelol+WHATTHESIGMA@gmail.com>
2025-06-06 19:32:34 +0800

    delete 2

--- pages/api/[code].js
+++
@@ -1,9 +0,0 @@
-let links = {}
-
-export default async function handler(req, res) {
-  const code = req.query.code || req.url.split('/').pop()
-  if (!code || !links[code]) return res.status(404).send('not found')
-
-  res.writeHead(302, { Location: links[code] })
-  res.end()
-}
