e3815c0a7599bbb1bc0dbce027f23fb6b7c68e7f
unstable <dthebestusernamelol+WHATTHESIGMA@gmail.com>
2025-06-06 19:25:58 +0800

    Update [code].js

--- pages/api/[code].js
+++ pages/api/[code].js
@@ -2,7 +2,7 @@ let links = {}
 
 export default async function handler(req, res) {
   const code = req.query.code || req.url.split('/').pop()
-  if (!code || !links[code]) return res.status(404).send('Not found')
+  if (!code || !links[code]) return res.status(404).send('not found')
 
   res.writeHead(302, { Location: links[code] })
   res.end()
